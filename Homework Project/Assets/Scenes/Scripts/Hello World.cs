c08740ea35fd286a3f7f18f513cc4e52c56e3673
sylv925 <sylv@udel.edu>
2024-09-02 12:35:35 -0400

    Completes Part C

--- Homework Project/Assets/Scenes/Scripts/Hello World.cs
+++ Homework Project/Assets/Scenes/Scripts/Hello World.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class HelloWorld : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, world!");
+        Debug.Log("Hello, World!");
+        Debug.LogWarning("Hello, I am a warning!");
+        Debug.LogError("I am an Error");
+        //AuthorSylv
+        /*
+        *I hope to learn basic coding!
+        *How long should we expect to work on projects outside of class each week?
+        *Until Then is a visual novel set in the Philipines with themes of grief and growing up.
+        */
     }
 
     // Update is called once per frame
